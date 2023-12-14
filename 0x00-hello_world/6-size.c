fbb0693ce0b6adf1f900b18a01338662731f3453
kedakaramm <ahmedsaidwork13@gmail.com>
2023-12-14 12:37:04 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	printf("size of a int: %d byte(s)\n", sizeof(int));
+	printf("size of a double: %d byte(s)\n", sizeof(double));
+	return (0);
+}
