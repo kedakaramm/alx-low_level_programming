2015aef25ff2bc068782106fe92ebb9937ce3155
kedakaramm <ahmedsaidwork13@gmail.com>
2023-12-14 07:44:11 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Succec)
+ *
+ * this is my first document
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
